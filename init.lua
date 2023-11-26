c82d6b8d15b1fba6d9da46c6a801fecca0c4fb00
lsmda <lsmda@apollo.pm>
2023-11-26 16:01:08 +0000

    add namespace

--- init.lua
+++ init.lua
@@ -1,12 +1,1 @@
-local options = {
-	autoindent = true,
-	smartindent = true,
-	tabstop = 2,
-	shiftwidth = 2,
-	expandtab = true,
-	showtabline = 0,
-
-	number = true
-}
-
-for option, value in pairs(options) do vim.opt[option] = value end
+require("lsmda")
