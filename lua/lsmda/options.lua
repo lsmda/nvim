07a1a38a6e99f6a249f44521e192fcf18f40ed79
lsmda <lsmda@apollo.pm>
2023-11-26 16:16:46 +0000

    update options

--- lua/lsmda/options.lua
+++ lua/lsmda/options.lua
@@ -6,7 +6,27 @@ local options = {
 	expandtab = true,
 	showtabline = 0,
 
-	number = true
+	number = true,
+  numberwidth = 4,
+  incsearch = true,
+  hlsearch = true,
+  ignorecase = true,
+  smartcase = true,
+
+  splitbelow = true,
+  splitright = true,
+
+  termguicolors = true,
+  hidden = true,
+  signcolumn = "no",
+  showmode = false,
+  wrap = false,
+  cursorline = true,
+  fileencoding = "utf-8",
+
+  backup = false,
+  title = true,
 }
 
 for option, value in pairs(options) do vim.opt[option] = value end
+
