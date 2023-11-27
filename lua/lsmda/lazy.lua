7b10d021f1d8d425513a148e86e1649134284f81
lsmda <lsmda@apollo.pm>
2023-11-27 12:20:01 +0000

    change default theme

--- lua/lsmda/lazy.lua
+++ lua/lsmda/lazy.lua
@@ -16,10 +16,10 @@ require("lazy").setup({
 })
 
 require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    flavour = "macchiato", -- latte, frappe, macchiato, mocha
     background = { -- :h background
         light = "latte",
-        dark = "mocha",
+        dark = "macchiato",
     },
     transparent_background = false, -- disables setting the background color.
     show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
