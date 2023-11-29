899ce93738abca522a52b1e9dc33128d618c8476
lsmda <lsmda@apollo.pm>
2023-11-29 14:21:51 +0000

    update harpoon keymaps

--- lua/lsmda/remap.lua
+++ lua/lsmda/remap.lua
@@ -1,8 +1,12 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+-- set <space> as the leader key
+-- see `:help mapleader`
+--  NOTE: must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- custom shortcuts
 vim.keymap.set("i", "<A-n>", "<Esc>")
+-- vim.keymap.set("n", "<A-e>", "")
+
+-- clear input for regular expression search in current buffer
+vim.api.nvim_set_keymap('n', 'cx', ':let @/=""<CR>', { noremap = true, silent = true })
