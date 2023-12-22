2bc2291c1c55da6c7e3cd5d42abc5d444ef6ee63
lsmda <lsmda@apollo.pm>
2023-12-22 18:42:45 +0000

    updates

--- lua/remap.lua
+++ lua/remap.lua
@@ -5,11 +5,8 @@ vim.g.maplocalleader = ' '
 -- [[ custom shortcuts ]]
 vim.keymap.set("i", "<M-n>", "<Esc>", { desc = 'Enter normal mode', noremap = true, silent = true })
 
--- clear input for regular expression search in current buffer
-vim.api.nvim_set_keymap('n', '<M-cs>', ':let @/=""<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-k>', 'yypk==', { desc = 'Clone current line above', noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-j>', 'yyPk==', { desc = 'Clone current line below', noremap = true, silent = true })
 
-vim.api.nvim_set_keymap('n', '<M-k>', 'yypk', { desc = 'Clone current line above', noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<M-j>', 'yyPj', { desc = 'Clone current line below', noremap = true, silent = true })
-
-vim.api.nvim_set_keymap('v', '<M-k>', 'yp', { desc = 'Clone current line above', noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<M-j>', 'yP', { desc = 'Clone current line below', noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<M-k>', 'yp==', { desc = 'Clone current line above', noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<M-j>', 'yP==', { desc = 'Clone current line below', noremap = true, silent = true })
