42cc5caf619d4b1541ef50ab62d005be88744371
lsmda <lsmda@apollo.pm>
2024-01-01 12:19:21 +0000

    update remaps

--- lua/remap.lua
+++ lua/remap.lua
@@ -7,12 +7,27 @@ local opts = { noremap = true, silent = true }
 -- Enter normal mode
 vim.keymap.set("i", "<m-n>", "<esc>", opts)
 
--- Clone current line above/below
-vim.keymap.set({ "n", "v" }, "<M-k>", "yypk==", opts)
-vim.keymap.set({ "n", "v" }, "<M-j>", "yyPj==", opts)
+-- Move line up/down
+vim.keymap.set("n", "<M-k>", ":m .-2<CR>==", opts)     -- Move current line up
+vim.keymap.set("n", "<M-j>", ":m .+1<CR>==", opts)     -- Move current line down
+vim.keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv", opts) -- Move current selection up
+vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv", opts) -- Move current selection down
+
+-- Duplicate line up/down
+vim.keymap.set("n", "<S-M-k>", ":t .-1<CR>==", opts) -- Duplicate current line up
+vim.keymap.set("n", "<S-M-j>", ":t .<CR>==", opts)   -- Duplicate current line down
+-- vim.keymap.set("v", "<S-M-k>", ":'<,'>t .-1<CR>gv=gv", opts)  -- Duplicate current selection up
+-- vim.keymap.set("v", "<S-M-j>", ":'<,'>t '>+1<CR>gv=gv", opts) -- Duplicate current selection down
+
+-- 1
+-- 2
+-- 3
 
 -- Pane navigation
 vim.keymap.set({ "n", "v" }, "<C-h>", ":TmuxNavigateLeft<CR>", opts)
 vim.keymap.set({ "n", "v" }, "<C-l>", ":TmuxNavigateRight<CR>", opts)
 vim.keymap.set({ "n", "v" }, "<C-j>", ":TmuxNavigateDown<CR>", opts)
 vim.keymap.set({ "n", "v" }, "<C-k>", ":TmuxNavigateUp<CR>", opts)
+
+-- Paste clipboard content without storing changed content
+vim.keymap.set({ "v" }, "p", [["_dP]], opts)
