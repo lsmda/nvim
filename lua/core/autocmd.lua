e759237a760831fb47193b3d1b73079755d4cd88
lsmda <lsmda@apollo.pm>
2024-01-23 10:54:57 +0000

    refactor: update autosave command

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,10 +1,11 @@
 local api = vim.api
+local utils = require("core.utils")
 
--- don't auto comment new line
+-- Don't auto comment new line
 api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
 
 -- Highlight yanked text
-vim.api.nvim_create_autocmd("TextYankPost", {
+api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
 		vim.highlight.on_yank({
 			higroup = "IncSearch",
@@ -13,47 +14,19 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
--- show cursor line only in active window
+-- Show cursor line in active window
 local cursorGrp = api.nvim_create_augroup("CursorLine", { clear = true })
-api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
-	pattern = "*",
-	command = "set cursorline",
-	group = cursorGrp,
-})
+api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, { pattern = "*", command = "set cursorline", group = cursorGrp })
 api.nvim_create_autocmd(
 	{ "InsertEnter", "WinLeave" },
 	{ pattern = "*", command = "set nocursorline", group = cursorGrp }
 )
 
--- Function to save the current file
-local function save_file()
-	-- Check for the specific conditions where we don't want to save
-	local is_floating_window_open = false
-	for _, win in pairs(vim.api.nvim_list_wins()) do
-		local config = vim.api.nvim_win_get_config(win)
-		if config.relative ~= "" then
-			is_floating_window_open = true
-			break
-		end
-	end
+-- Auto-save file on change
+local save_file = utils.save_file
 
-	-- Save only if the buffer is modifiable, has been modified, not readonly, not a new buffer (buftype is empty),
-	-- and no floating windows (like Telescope) are open
-	if
-		vim.bo.modifiable
-		and vim.bo.modified
-		and not vim.bo.readonly
-		and vim.bo.buftype == ""
-		and not is_floating_window_open
-	then
-		vim.cmd("silent write")
-	end
-end
-
--- Set up an auto-command group to prevent duplicate autocmds
-vim.api.nvim_create_augroup("autosave", {})
-
-vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI", "InsertLeave" }, {
+api.nvim_create_augroup("autosave", {})
+api.nvim_create_autocmd({ "TextChanged", "TextChangedI", "InsertLeave" }, {
 	group = "autosave",
 	pattern = "*",
 	callback = function()
--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -56,4 +56,28 @@ end
 
 M.opts = { noremap = true, silent = true }
 
+M.save_file = function()
+	-- Check for the specific conditions where we don't want to save
+	local is_floating_window_open = false
+	for _, win in pairs(vim.api.nvim_list_wins()) do
+		local config = vim.api.nvim_win_get_config(win)
+		if config.relative ~= "" then
+			is_floating_window_open = true
+			break
+		end
+	end
+
+	-- Save only if the buffer is modifiable, has been modified, not readonly, not a new buffer (buftype is empty),
+	-- and no floating windows (like Telescope) are open
+	if
+		vim.bo.modifiable
+		and vim.bo.modified
+		and not vim.bo.readonly
+		and vim.bo.buftype == ""
+		and not is_floating_window_open
+	then
+		vim.cmd("silent write")
+	end
+end
+
 return M
