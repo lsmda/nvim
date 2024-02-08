9d88f65fddc7af911b2bc8499de7670e775ad272
lsmda <lsmda@apollo.pm>
2024-02-08 14:18:01 +0000

    update nvim-lint

--- lua/plugins/lsp/nvim-lint.lua
+++ lua/plugins/lsp/nvim-lint.lua
@@ -1,6 +1,23 @@
 return {
 	"mfussenegger/nvim-lint",
 	commit = "2cf9ad095130755d7d87f1730bcf33c91ee822e4",
+	opts = {
+		-- other config
+		linters = {
+			eslint_d = {
+				args = {
+					"--no-warn-ignored", -- <-- this is the key argument
+					"--format",
+					"json",
+					"--stdin",
+					"--stdin-filename",
+					function()
+						return vim.api.nvim_buf_get_name(0)
+					end,
+				},
+			},
+		},
+	},
 	config = function()
 		require("lint").linters_by_ft = {
 			astro = { "eslint_d" },
