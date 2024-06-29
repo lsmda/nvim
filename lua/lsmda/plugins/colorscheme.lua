a3683a817022c989662724f803f10f933ade51c6
lsmda <lsmda@apollo.pm>
2024-06-29 23:37:12 +0100

    update colorscheme

--- lua/lsmda/plugins/colorscheme.lua
+++ lua/lsmda/plugins/colorscheme.lua
@@ -4,7 +4,9 @@ return {
 	priority = 1000,
 	config = function()
 		vim.g.everforest_better_performance = 1
+		vim.g.everforest_background = "soft"
 		vim.g.everforest_transparent_background = 2
+		vim.g.everforest_disable_terminal_colors = 1
 		vim.g.everforest_ui_contrast = "high"
 		vim.g.everforest_float_style = "dim"
 		vim.cmd.colorscheme("everforest")
