0ef4731a7b951b562daed15b51e79918db61dbf4
lsmda <lsmda@apollo.pm>
2024-07-14 00:36:33 +0100

    update keymaps

--- lua/lsmda/core/keymaps.lua
+++ lua/lsmda/core/keymaps.lua
@@ -35,6 +35,13 @@ vim.keymap.set("n", "<leader>cs", function()
 	vim.fn.setreg("/", "")
 end, { desc = "Clear search pattern" })
 
+vim.keymap.set("n", "<leader>'", [[:s/\<<C-r><C-w>\>/'<C-r><C-w>\'/ <CR>]], { desc = "Surround word with '" })
+vim.keymap.set("n", '<leader>"', [[:s/\<<C-r><C-w>\>/"<C-r><C-w>\"/ <CR>]], { desc = 'Surround word with "' })
+vim.keymap.set("n", "<leader>*", [[:s/\<<C-r><C-w>\>/*<C-r><C-w>\*/ <CR>]], { desc = "Surround word with *" })
+vim.keymap.set("n", "<leader>(", [[:s/\<<C-r><C-w>\>/(<C-r><C-w>\)/ <CR>]], { desc = "Surround word with (" })
+vim.keymap.set("n", "<leader>[", [[:s/\<<C-r><C-w>\>/[<C-r><C-w>\]/ <CR>]], { desc = "Surround word with [" })
+vim.keymap.set("n", "<leader>{", [[:s/\<<C-r><C-w>\>/{<C-r><C-w>\}/ <CR>]], { desc = "Surround word with {" })
+
 -- insert mode
 vim.keymap.set("i", "<C-h>", "<Left>", { desc = "Navigate left in insert mode" })
 vim.keymap.set("i", "<C-l>", "<Right>", { desc = "Navigate right in insert mode" })
@@ -42,20 +49,21 @@ vim.keymap.set("i", "<C-j>", "<Down>", { desc = "Navigate down in insert mode" }
 vim.keymap.set("i", "<C-k>", "<Up>", { desc = "Navigate up in insert mode" })
 
 -- visual mode
-vim.keymap.set("v", "'", "c''<Esc>P", { desc = "Surround selection with single quotes" })
-vim.keymap.set("v", '"', 'c""<Esc>P', { desc = "Surround selection with double quotes" })
-vim.keymap.set("v", "(", "c()<Esc>P", { desc = "Surround selection with parenthesis" })
-vim.keymap.set("v", "[", "c[]<Esc>P", { desc = "Surround selection with brackets" })
-vim.keymap.set("v", "{", "c{}<Esc>P", { desc = "Surround selection with curly braces" })
+vim.keymap.set("x", "'", "c''<Esc>P", { desc = "Surround selection with '" })
+vim.keymap.set("x", '"', 'c""<Esc>P', { desc = 'Surround selection with "' })
+vim.keymap.set("x", "*", "c**<Esc>P", { desc = "Surround selection with *" })
+vim.keymap.set("x", "(", "c()<Esc>P", { desc = "Surround selection with (" })
+vim.keymap.set("x", "[", "c[]<Esc>P", { desc = "Surround selection with [" })
+vim.keymap.set("x", "{", "c{}<Esc>P", { desc = "Surround selection with {" })
 
-vim.keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
-vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("x", "<M-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+vim.keymap.set("x", "<M-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 
-vim.keymap.set("v", "<S-M-k>", "<cmd>VisualDuplicate -1<CR>", { desc = "Duplicate selection up" })
-vim.keymap.set("v", "<S-M-j>", "<cmd>VisualDuplicate +1<CR>", { desc = "Duplicate selection down" })
+vim.keymap.set("x", "<S-M-k>", "<cmd>VisualDuplicate -1<CR>", { desc = "Duplicate selection up" })
+vim.keymap.set("x", "<S-M-j>", "<cmd>VisualDuplicate +1<CR>", { desc = "Duplicate selection down" })
 
 -- mixed modes
-vim.keymap.set({ "i", "v" }, "<M-n>", "<Esc>", { desc = "Enter normal mode" })
-vim.keymap.set({ "n", "v" }, "<M-e>", "$", { desc = "Move cursor to end of line" })
-vim.keymap.set({ "n", "v" }, "<M-q>", "^", { desc = "Move cursor to beginning of line" })
-vim.keymap.set({ "n", "v" }, "<leader>rr", "<cmd>source %<CR>", { desc = "Resource configuration" })
+vim.keymap.set({ "i", "x" }, "<M-n>", "<Esc>", { desc = "Enter normal mode" })
+vim.keymap.set({ "n", "x" }, "<M-e>", "$", { desc = "Move cursor to end of line" })
+vim.keymap.set({ "n", "x" }, "<M-q>", "^", { desc = "Move cursor to beginning of line" })
+vim.keymap.set({ "n", "x" }, "<leader>rr", "<cmd>source %<CR>", { desc = "Resource configuration" })
