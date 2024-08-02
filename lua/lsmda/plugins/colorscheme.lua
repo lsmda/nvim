4bcc9fe17ded25e2723d134ce5c088084c69bc40
lsmda <contact@lsmda.pm>
2024-08-02 23:38:21 +0100

    update theme

--- lua/lsmda/plugins/colorscheme.lua
+++ lua/lsmda/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
-	"tinted-theming/base16-vim",
+	"sainnhe/everforest",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.g.base16_colorspace = 256
-		vim.g.base16_background_transparent = 1
-		vim.cmd.colorscheme("bubblegum")
+		vim.g.everforest_better_performance = 1
+		vim.g.everforest_transparent_background = 2
+		vim.g.everforest_ui_contrast = "high"
+		vim.g.everforest_float_style = "dim"
+		vim.cmd.colorscheme("everforest")
 	end,
 }
