82af70b784353de54ab5ba5f05bf6eead0d00e34
lsmda <contact@lsmda.pm>
2024-08-16 21:43:56 +0100

    update colorscheme

--- lua/lsmda/plugins/colorscheme.lua
+++ lua/lsmda/plugins/colorscheme.lua
@@ -1,12 +1,18 @@
 return {
-	"sainnhe/everforest",
-	lazy = false,
-	priority = 1000,
+	-- "sainnhe/everforest",
+	-- lazy = false,
+	-- priority = 1000,
+	-- config = function()
+	-- 	vim.g.everforest_better_performance = 1
+	-- 	vim.g.everforest_transparent_background = 2
+	-- 	vim.g.everforest_ui_contrast = "high"
+	-- 	vim.g.everforest_float_style = "dim"
+	-- 	vim.cmd.colorscheme("everforest")
+	-- end,
+
+	-- Using lazy.nvim
+	"mellow-theme/mellow.nvim",
 	config = function()
-		vim.g.everforest_better_performance = 1
-		vim.g.everforest_transparent_background = 2
-		vim.g.everforest_ui_contrast = "high"
-		vim.g.everforest_float_style = "dim"
-		vim.cmd.colorscheme("everforest")
+		vim.cmd("colorscheme mellow")
 	end,
 }
