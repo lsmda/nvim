c14a693466876cbb2ab63c02662edd307a5677cb
lsmda <contact@lsmda.pm>
2024-10-07 10:19:12 +0100

    update colorscheme

--- lua/lsmda/plugins/colorscheme.lua
+++ lua/lsmda/plugins/colorscheme.lua
@@ -1,28 +1,25 @@
 return {
+	"sainnhe/everforest",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.g.everforest_better_performance = 1
+		vim.g.everforest_transparent_background = 2
+		vim.g.everforest_ui_contrast = "high"
+		vim.g.everforest_float_style = "dim"
+		vim.cmd.colorscheme("everforest")
+	end,
 
-	-- "sainnhe/everforest",
+	-- "projekt0n/github-nvim-theme",
 	-- lazy = false,
 	-- priority = 1000,
 	-- config = function()
-	-- 	vim.g.everforest_better_performance = 1
-	-- 	vim.g.everforest_transparent_background = 2
-	-- 	vim.g.everforest_ui_contrast = "high"
-	-- 	vim.g.everforest_float_style = "dim"
-	-- 	vim.cmd.colorscheme("everforest")
+	-- 	require("github-theme").setup({
+	-- 		options = {
+	-- 			transparent = true,
+	-- 		},
+	-- 	})
+	--
+	-- 	vim.cmd.colorscheme("github_dark_tritanopia")
 	-- end,
-
-	{
-		"projekt0n/github-nvim-theme",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("github-theme").setup({
-				options = {
-					transparent = true,
-				},
-			})
-
-			vim.cmd("colorscheme github_dark_tritanopia")
-		end,
-	},
 }
